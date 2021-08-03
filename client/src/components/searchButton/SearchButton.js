659c6f85b48348982e1fc49d14f103acf61abcb1
Juampi <juanpablo.cantuna@gmail.com>
2021-08-03 18:40:29 +0200

    Added the searchbutton css file

--- client/src/components/searchButton/SearchButton.js
+++ client/src/components/searchButton/SearchButton.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState,useContext } from 'react'
+import {valuesContext} from '../../contexts/weather.contexts'
 import './SearchButton.css'
 
 const SearchButton = () =>{
-
+    const { searcher, setSearcher } = useContext(valuesContext)
+    const activateSearch = () =>{
+        setSearcher(!searcher)
+    }
     return (
         <section className='containerBSP'>
-            <button className='searchPlacesBtn'>Search for places</button>
+            <button className='searchPlacesBtn' onClick={activateSearch} >Search for places</button>
         </section>
     );
 }
