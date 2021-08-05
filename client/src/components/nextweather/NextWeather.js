bc859430ba80f47c4a424ba23b729be18e3dd2cc
Juampi <juanpablo.cantuna@gmail.com>
2021-08-05 02:41:08 +0200

    Added the daylist function to print the weather of the next five days

--- client/src/components/nextweather/NextWeather.js
+++ client/src/components/nextweather/NextWeather.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import NextW from '../nextweatherlist/Nextweatherlist'
+import { valuesContext } from '../../contexts/weather.contexts'
 import './NextWeather.css'
+
 const NextWeather = () =>{
+
+
+    const { city, dataWeather } = useContext(valuesContext)
+    const [ nextDaysData, setNextDaysData] = useState([])
+
+    const renderNextDaysWeather = () =>{
+        if(nextDaysData){
+            /* console.log('dtawEATHERLIST:',nextDaysData); */
+            return <NextW data={nextDaysData}/>
+        }
+    }
+
+    const renderTodayHL = () =>{
+        if(nextDaysData){
+            console.log('dtawHIGHTLIGHT:',nextDaysData)
+        }
+    } 
+
+    useEffect(()=>{
+        if(dataWeather.length !== 0){
+            let tmpA = dataWeather.consolidated_weather
+            setNextDaysData(tmpA)
+/*             renderNextDaysWeather()
+            renderTodayHL() */
+        }
+    },[dataWeather])
+
+
     return(
-        <section className="containerNW">
-            <section >
-                <span>hola</span>
+        <section className='containerNW'>
+            <section className='containerNextListW'>
+                {renderNextDaysWeather()}
+            </section>
+            <section className='containerHightLights'>
+                <section className='titleHightLights'>
+                    <span>Today's Hightlights</span>
+                </section>
+                <section className='listHightLights'>
+                    <p>LA LISTA DE LOS Hightlights</p>
+                </section>
             </section>
         </section>
     );
